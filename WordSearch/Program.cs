bf46c6eb1a71f9ad9b4faa1d66d090abb31d0fb1
Alistair Grant <alistair@teal.kiwi>
2023-04-05 16:09:05 +1200

    Add simple find words feature

--- WordSearch/Program.cs
+++ WordSearch/Program.cs
@@ -1,11 +1,11 @@
 ﻿using System;
-using System.Data;
 
 namespace WordSearch
 {
     class Program
     {
-        static char[,] Grid = new char[,] {
+        static char[,] Grid = new char[,]
+        {
             {'C', 'P', 'K', 'X', 'O', 'I', 'G', 'H', 'S', 'F', 'C', 'H'},
             {'Y', 'G', 'W', 'R', 'I', 'A', 'H', 'C', 'Q', 'R', 'X', 'K'},
             {'M', 'A', 'X', 'I', 'M', 'I', 'Z', 'A', 'T', 'I', 'O', 'N'},
@@ -40,6 +40,11 @@ namespace WordSearch
             "WINDOW"
         };
 
+        static int[,] Deltas = new int[,]
+        {
+            { -1, -1 }, { -1, 0 }, { -1, 1 }, { 0, -1 }, { 0, 1 }, { 1, -1 }, { 1, 0 }, { 1, 1 }
+        };
+
         static void Main(string[] args)
         {
             Console.WriteLine("Word Search");
@@ -69,9 +74,86 @@ namespace WordSearch
 
         private static void FindWords()
         {
-            //Find each of the words in the grid, outputting the start and end location of
-            //each word, e.g.
-            //PUPPY found at (10,7) to (10, 3) 
+            foreach (string word in Words)
+            {
+                for (int startY = 0; startY < 12; startY++)
+                {
+                    for (int startX = 0; startX < 12; startX++)
+                    {
+                        int endX = startX;
+                        int endY = startY;
+
+                        if (Matches(word, ref endX, ref endY))
+                        {
+                            Console.WriteLine($"{word} found at ({startX}, {startY}) to ({endX}, {endY})");
+                        }
+                    }
+                }
+            }
+        }
+
+        private static bool Matches(char letter, int x, int y)
+        {
+            if (OutOfRange(x, y))
+            {
+                return false;
+            }
+
+            return letter == Grid[y, x];
+        }
+
+        private static bool Matches(string word, ref int endX, ref int endY)
+        {
+            int startX = endX;
+            int startY = endY;
+
+            for (int i = 0; i < 8; i++)
+            {
+                int deltaX = Deltas[i, 1];
+                int deltaY = Deltas[i, 0];
+
+                endX = startX;
+                endY = startY;
+
+                if (Matches(word, ref endX, ref endY, deltaX, deltaY))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        private static bool Matches(string word, ref int endX, ref int endY, int deltaX, int deltaY)
+        {
+            int length = word.Length;
+
+            return Matches(word, 0, length, ref endX, ref endY, deltaX, deltaY);
+        }
+
+        private static bool Matches(string word, int startIndex, int length, ref int endX, ref int endY, int deltaX, int deltaY)
+        {
+            char letter = word[startIndex];
+            if (!Matches(letter, endX, endY))
+            {
+                return false;
+            }
+
+            startIndex++;
+            if (startIndex >= length)
+            {
+                return true;
+            }
+
+            endX += deltaX;
+            endY += deltaY;
+
+            return Matches(word, startIndex,length, ref endX, ref endY, deltaX, deltaY);
+        }
+
+        private static bool OutOfRange(int x, int y)
+        {
+            return x < 0 || x >= 12 || y < 0 || y >= 12;
         }
     }
 }
